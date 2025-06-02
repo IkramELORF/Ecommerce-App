d879e86d834549fe00a3aa44d68d583e7f2748ff
IkramELORF <ikramelorf@gmail.com>
2025-06-02 15:26:29 +0200

    06: categories finalization

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -5,6 +5,7 @@ import { Footer } from "./footer";
 import { Navbar } from "./navbar";
 import { SearchFilters } from "./search-filters";
 import { Category } from '@/payload-types';
+import { CustomCategory } from './types';
 
 interface Props {
     children: React.ReactNode;
@@ -23,16 +24,16 @@ const Layout = async ({ children }: Props) => {
             exists:false,
           },
         },
+        sort: "name"
       });
-      const formattedData = data.docs.map((doc)=>({
+      const formattedData: CustomCategory[] = data.docs.map((doc)=>({
         ...doc,
         subcategories: (doc.subcategories?.docs ?? []).map((doc)=>({
             // Because of "depth:1" we are confident "doc" will be a type of "Category"
-            ...(doc as Category),
+            ...(doc as CustomCategory),
             subcategories: undefined,
         }))
       }));
-    console.log(data,formattedData );
     return (
         <div className="flex flex-col min-h-screen">
             <Navbar />
