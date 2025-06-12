e99d58ac7cbe4a3022bd19593672306d7e632227
IkramELORF <ikramelorf@gmail.com>
2025-06-12 23:42:56 +0200

    18: cart and checkout

--- src/modules/tenants/ui/components/navbar.tsx
+++ src/modules/tenants/ui/components/navbar.tsx
@@ -6,11 +6,22 @@ import Link from "next/link";
 import Image from "next/image";
 import { generateTenantURL } from "@/lib/utils";
 
+import dynamic from "next/dynamic";
+import { Button } from "@/components/ui/button";
+import { ShoppingCartIcon } from "lucide-react";
+
+const CheckoutButton = dynamic(
+    () => import("@/modules/checkout/ui/components/checkout-button").then((mod) => mod.CheckoutButton),
+    {
+        ssr: false,
+        loading: () => (<Button disabled className="bg-white"><ShoppingCartIcon className="text-black"/></Button>)
+    },
+);
 interface Props {
     slug: string;
 };
 
-export const Navbar = ({slug}: Props)=>{
+export const Navbar = ({ slug }: Props) => {
     const trpc = useTRPC();
     const { data } = useSuspenseQuery(trpc.tenants.getOne.queryOptions({ slug }));
     return (
@@ -18,27 +29,30 @@ export const Navbar = ({slug}: Props)=>{
             <div className="max-w-(--breakpoint-xl) mx-auto flex justify-between items-center h-full px-4 lg:px-12">
                 <Link href={generateTenantURL(slug)} className="flex items-center gap-2">
                     {data.image?.url && (
-                        <Image 
-                        src={data.image.url}
-                        width={32}
-                        height={32}
-                        className="rounded-full shrink-0 size-[32px]"
-                        alt={slug}
+                        <Image
+                            src={data.image.url}
+                            width={32}
+                            height={32}
+                            className="rounded-full shrink-0 size-[32px]"
+                            alt={slug}
                         />
                     )}
                     <p className="text-xl">{data.name}</p>
-                 </Link>
+                </Link>
+                <CheckoutButton tenantSlug={slug} />
             </div>
         </nav>
     );
 };
 
-export const NavbarSkeleton = () =>{
-    return(
+export const NavbarSkeleton = () => {
+    return (
         <nav className="h-20 border-b font-medium bg-white">
             <div className="max-w-(--breakpoint-xl) mx-auto flex justify-between items-center h-full px-4 lg:px-12">
-            <div />
-            {/* TODO: SKeleton for checkout button*/}
+                <div />
+                <Button disabled className="bg-white">
+                    <ShoppingCartIcon className="text-black"/>
+                </Button>
             </div>
         </nav>
     );
