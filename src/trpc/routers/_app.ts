f558d6894880166c730cccca2d7181f7aa8690cc
IkramELORF <ikramelorf@gmail.com>
2025-06-13 20:24:45 +0200

    19: checkout page

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -4,9 +4,11 @@ import { categoriesRouter } from '@/modules/categories/server/procedures';
 import { productsRouter } from '@/modules/products/server/procedures';
 import { tagsRouter } from '@/modules/tags/server/procedures';
 import { tenantsRouter } from '@/modules/tenants/server/procedures';
+import { checkoutRouter } from '@/modules/checkout/server/procedures';
 
 export const appRouter = createTRPCRouter({
     auth: authRouter,
+    checkout: checkoutRouter,
     tenants: tenantsRouter,
     tags: tagsRouter,
     products: productsRouter,
